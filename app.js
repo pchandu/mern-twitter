9aa421cac6d754d89685d23d9ac7f73c003cd2a8
Praneeth Chandu <praneethchandu@gmail.com>
2020-10-26 00:35:31 -0700

    added user auth

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const bodyParser = require('body-parser');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
@@ -9,13 +10,14 @@ mongoose
 const port = process.env.PORT || 5000;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
-const bodyParser = require('body-parser');
+const passport = require('passport');
 
 
-app.get("/", (req, res) => res.send("Hello, this is Praneeth."));
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(passport.initialize());
+require('./config/passport')(passport);
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
